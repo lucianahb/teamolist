6a11da5fffeeeb31141e223074fc0e3ddc8c73e0
Gustavo Sobreiro de Assis <gustavo.sobreiro@olist.com>
2021-01-04 17:04:35 -0300

    [Frontend]Conclusao do cadastro de marketplace e produtos

--- main_web.py
+++ main_web.py
@@ -1,13 +1,41 @@
 from flask import Flask, render_template, request
 from funcoes import escrever_arquivo
 
-
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
 @app.route('/')
 def index():
+    return render_template('index.html', titulo='Marketplace Olist')
+
+
+@app.route('/cadastro')
+def view_cadastro():
+    opcao = request.args.get('opcao')
+
+    if opcao == 'marketplace':
+        return render_template('cadastro.html', titulo='Marketplace', op=opcao)
+    elif opcao == 'produto':
+        return render_template('cadastro.html', titulo='Produto', op=opcao)
+    else:
+        return render_template('index.html', titulo='Marketplace Olist')
+
+
+@app.route('/gravar')
+def gravar_dados():
+    nome = request.args.get('nome')
+    desc = request.args.get('descricao')
+    preco = request.args.get('preco')
+
+
+    if preco is None:
+        dado = f'{nome},{desc}'
+        escrever_arquivo(dado, 0, 'a')
+    else:
+        dado = f'{nome},{desc},{preco}'
+        escrever_arquivo(dado, 1, 'a')
+
     return render_template('index.html', titulo='Marketplace Olist')
 
 
