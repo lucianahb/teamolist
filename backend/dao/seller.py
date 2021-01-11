5b1a5be376dc8a8615256314799a462fb339e71e
victorhausen <hausen.victor@gmail.com>
2021-01-11 12:21:06 -0300

    adicao dos DAOs

---
+++ backend/dao/seller.py
@@ -0,0 +1,19 @@
+root = '../backend/files/list_seller.txt'
+
+def create_seller(data:str):
+    try:
+        with open(root, 'a') as file:
+            file.write(data+'\n')
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+def read_sellers():
+    list_seller = []
+    file_seller = open(root, 'r')
+    for f in  file_seller:
+        seller = f.strip().split(';')
+        list_seller.append(seller)
+    return list_seller
