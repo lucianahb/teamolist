4bf084dafea2732a5d92b80a55fe628b172f3945
luciheit@gmail.com <luciheit@gmail.com>
2021-01-11 10:48:29 -0300

    inserção de cores para listagem do log

--- frontend/main_web.py
+++ frontend/main_web.py
@@ -44,7 +44,8 @@ def save_mkp():
     desc = request.args.get('descricao')
     data = f'{nome};{desc}'
     save_mkplace(data)
-    save_log('Saved Marketplace')
+    operation_type = 1 #1=save and 2=list
+    save_log('Saved Marketplace', operation_type)
     return redirect('/list-mkp')
 
 
@@ -55,7 +56,8 @@ def save_prod():
     preco = request.args.get('preco')
     data = f'{nome};{desc};{preco}'
     save_product(data)
-    save_log('Saved Product')
+    operation_type = 1 #1=save and 2=list
+    save_log('Saved Product', operation_type)
     return redirect('/list-product')
 
 
@@ -66,7 +68,8 @@ def save_sel():
     telefone = request.args.get('telefone')
     data = f'{nome};{email};{telefone}'
     save_seller(data)
-    save_log('Saved Seller')
+    operation_type = 1 #1=save and 2=list
+    save_log('Saved Seller', operation_type)
     return redirect('/list-seller')
 
 
@@ -75,35 +78,42 @@ def save_cat():
     nome = request.args.get('nome')
     data = f'{nome}'
     save_category(data)
-    save_log('Saved Category')
+    operation_type = 1 #1=save and 2=list
+    save_log('Saved Category', operation_type)
     return redirect('/list-category')
 
 
 @app.route('/list-mkp')
 def list_mkp():
     final_list = list_mkplaces()
-    save_log('Listed Marketplace')
-    return render_template('list_mkp.html', list=final_list)
+    operation_type = 2 #1=save and 2=list
+    save_log('Listed Marketplace', operation_type)
+    return render_template('list_mkp.html', list=final_list, save_log=save_log)
 
 
 @app.route('/list-product')
 def list_product():
     final_list = list_products()
-    save_log('Listed Product')
-    return render_template('list_product.html', list=final_list)
+    operation_type = 2 #1=save and 2=list
+    save_log('Listed Product', 2)
+    return render_template('list_product.html', list=final_list, save_log=save_log)
+
 
 @app.route('/list-seller')
 def list_seller():
     final_list = list_sellers()
-    save_log('Listed Seller')
-    return render_template('list_seller.html', list=final_list)
+    operation_type = 2 #1=save and 2=list
+    save_log('Listed Seller', 2)
+    return render_template('list_seller.html', list=final_list, save_log=save_log)
 
 
 @app.route('/list-category')
 def list_category():
     final_list = list_categories()
-    save_log('Listed Category')
-    return render_template('list_category.html', list=final_list)
+    operation_type = 2 #1=save and 2=list
+    save_log('Listed Category', operation_type)
+    return render_template('list_category.html', list=final_list, save_log=save_log)
+
 
 @app.route('/list-log')
 def list_log():
