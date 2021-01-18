8948916e4885ff9b1028495faa3ac9a3c50230ef
lucasljoliveira <lucasljoliveira@hotmail.com>
2021-01-18 11:55:14 -0300

    Adicionados getters e setters do model marketplace

--- backend/models/marketplace_model.py
+++ backend/models/marketplace_model.py
@@ -1,5 +1,29 @@
 class Marketplace:
     def __init__(self, name:str, description:str, id:int = None):
-        self.id = id
-        self.name = name
-        self.description = description
+        self.__id = id
+        self.__name = name
+        self.__description = description
+
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @property
+    def description(self):
+        return self.__description
+
+    @id.setter
+    def id(self, id):
+        self.__id = id
+
+    @name.setter
+    def name(self, name):
+        self.__name = name
+
+    @description.setter
+    def description(self, description):
+        self.__description = description
